5a52848e05c466056596fdc292084fabf06e2da2
Vitor Vechin <141504266+vitorvechin@users.noreply.github.com>
2026-09-16 16:57:10 -0300

    Add: count-number-of-islands - submission-1

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-1.py
@@ -0,0 +1,35 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        def explore(x, y):
+            # Stop condition
+            if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]):
+                return
+            
+            if grid[x][y] != "1":
+                return
+
+            # Visited
+            grid[x][y] = "0"
+
+            # Explore up
+            explore(x + 1, y)
+            
+            # Explore left
+            explore(x, y - 1)
+
+            # Explore right
+            explore(x, y + 1)
+
+            # Explore down
+            explore(x - 1, y)
+
+
+        islands = 0
+
+        for x in range(len(grid)):
+            for y in range(len(grid[0])):
+                if grid[x][y] == "1":
+                    islands += 1
+                    explore(x, y)
+
+        return islands
